2f623c77b4bed01d9a8fbced0e6399c9245cc208
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-12-02 23:15:10 +0100

    Pie chart for calorics

--- SportApp/Client/Helpers/PieChartData.cs
+++ SportApp/Client/Helpers/PieChartData.cs
@@ -7,16 +7,19 @@ namespace SportApp.Client.Helpers
 {
     public class PieChartData
     {
-        public PieChartData(int intValue, string strValue)
+        public PieChartData(int intValue, int calories, string strValue)
         {
             Value = intValue;
+            Calories = calories;
             Sport = strValue;
         }
+
         public PieChartData( string strValue)
         {            
             Sport = strValue;
         }
 
+        public int Calories { get; set; } = 0;
         public int Value { get; set; } = 0;
         public string Sport { get; set; }
 
