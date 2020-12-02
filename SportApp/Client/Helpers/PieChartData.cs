a02e114bce27dda42e3ffd79412ed68a4af5160d
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-12-02 22:09:30 +0100

    Pie Chart for number of training session

---
+++ SportApp/Client/Helpers/PieChartData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportApp.Client.Helpers
+{
+    public class PieChartData
+    {
+        public PieChartData(int intValue, string strValue)
+        {
+            Value = intValue;
+            Sport = strValue;
+        }
+        public PieChartData( string strValue)
+        {            
+            Sport = strValue;
+        }
+
+        public int Value { get; set; } = 0;
+        public string Sport { get; set; }
+
+    }
+}
